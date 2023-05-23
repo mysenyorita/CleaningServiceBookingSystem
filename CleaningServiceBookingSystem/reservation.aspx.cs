ad1d9e3ebb4b2246e291741b0f7e9d04dc3bbfb7
Emmanuel Cy Coyoca <emmanuelcycoyoca@ctu.edu.ph>
2023-05-23 15:16:09 +0800

    Update Reservation.aspx

--- CleaningServiceBookingSystem/reservation.aspx.cs
+++ CleaningServiceBookingSystem/reservation.aspx.cs
@@ -3,12 +3,17 @@ using System.Web.UI;
 
 namespace CleaningServiceBookingSystem
 {
-    public partial class reservation : Page
+    public partial class Reservation : Page
     {
-        protected void btnReserved_OnClick(object sender, EventArgs e)
+        protected void Page_Load(object sender, EventArgs e)
         {
-
+            
         }
        
+
+        protected void btnReserved_OnClick(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
